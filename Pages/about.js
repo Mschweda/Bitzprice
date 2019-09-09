a212120334d3306bf8565cc55cce219d1ea562ce
marisa l schweda <tahoe27@gmail.com>
2019-09-09 13:18:22 -0500

    Add files via upload

---
+++ Pages/about.js
@@ -0,0 +1,14 @@
+import Layout from '../Components/Layout';
+
+const About = () => (
+    <Layout>
+        <div>
+            <h1>About Bitz Prices</h1>
+            <p>Application to view Bitcoin prices</p>
+        </div>
+    </Layout>
+);
+
+
+
+export default About;
